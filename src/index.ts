6002914bf0a87a58fc8990d6c97c5652e41810fb
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-08-25 19:39:20 +0100

    adding graphql and express

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  app.get("/", (_, res) => {
+    res.send("Hello");
+  });
+
+  app.listen(4000, () =>
+    console.log(`Server started on http://localhost:4000`)
+  );
 };
 
 main().catch((err) => console.log(err));
