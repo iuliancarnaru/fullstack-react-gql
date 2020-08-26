ade19ea78ed1ed8af0383535fbfa2c476b03d663
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-08-26 23:20:08 +0100

    cleaning default structuree and add ts

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4ca521cb72de6231fc5f";
+/******/ 	var hotCurrentHash = "af9eb586e590cadd7ce9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
