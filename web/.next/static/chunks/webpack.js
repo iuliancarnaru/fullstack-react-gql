a2d285db48063e78d8093d05a42990aa80f7c4ed
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-09-01 20:26:39 +0100

    Updating packages

--- web/.next/static/chunks/webpack.js
+++ web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "149ff2f86f642616f9b9";
+/******/ 	var hotCurrentHash = "986b078d66957f18694f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
