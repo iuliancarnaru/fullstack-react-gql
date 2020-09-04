31a867e5b4591021183b7c412d0b1ae2223341a4
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-09-04 14:50:45 +0100

    adding change password on frontend p3

--- web/.next/static/development/_buildManifest.js
+++ web/.next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/change-password/[token]":["static\u002Fchunks\u002Fpages\u002Fchange-password\u002F[token].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
